4522b62d5296210bede25d5e1159d5d0180f4a73
Budry <zarubaondra@gmail.com>
2020-01-18 21:56:23 +0100

    Added authorization

--- cmd/root.go
+++ cmd/root.go
@@ -11,23 +11,35 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "prometheus-file-sd-updater-api",
+	Use:   "prometheus-file-sd-updater-api [path] [token]",
 	Short: "Add or remove hostname from Prometheus JSON file service discovery",
-	Args: cobra.ExactArgs(1),
+	Args: cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
+
+		requiredToken := args[1]
+
 		r := mux.NewRouter()
-		r.HandleFunc("/add/{hostname}", func(w http.ResponseWriter, r *http.Request) {
+		r.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
+			if isAuthorized(requiredToken, r) {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
 			vars := mux.Vars(r)
 			targetFile := prometheus.NewTargetFile(args[0])
 			targetFile.Append(vars["hostname"])
 			w.WriteHeader(http.StatusNoContent)
-		})
-		r.HandleFunc("/remove/{hostname}", func(w http.ResponseWriter, r *http.Request) {
+
+		}).Methods("POST")
+		r.HandleFunc("/remove", func(w http.ResponseWriter, r *http.Request) {
+			if isAuthorized(requiredToken, r) {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
 			vars := mux.Vars(r)
 			targetFile := prometheus.NewTargetFile(args[0])
 			targetFile.Remove(vars["hostname"])
 			w.WriteHeader(http.StatusNoContent)
-		})
+		}).Methods("POST")
 
 		port, err := cmd.Flags().GetString("port")
 		if err != nil {
@@ -45,4 +57,11 @@ func Execute() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func isAuthorized(requiredToken string, r *http.Request) bool {
+	token := r.Header.Get("Authorization")
+	token = token[7:len(token)]
+
+	return requiredToken != token
 }
